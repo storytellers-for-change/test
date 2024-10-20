8c49f707575e1f6b045c71d2393f4dc78d23a133
storytellers-for-change <steffi.kohl@zuyd.nl>
2024-10-20 23:00:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,27 +12,4 @@ function closeMenu() {
 }
 
 openMenuBtn.addEventListener('click', openMenu);
-closeMenuBtn.addEventListener('click', closeMenu);
-
-// Image Animation on Scroll
-const sections = document.querySelectorAll('.content-section');
-const images = document.querySelectorAll('.image');
-const imageContainer = document.querySelector('.image-container');
-
-window.addEventListener('scroll', () => {
-  let index = 0;
-  sections.forEach((section, i) => {
-    const rect = section.getBoundingClientRect();
-    if (rect.top <= window.innerHeight / 2) {
-      index = i;
-    }
-  });
-
-  images.forEach((img, i) => {
-    if (i === index) {
-      img.style.opacity = 1;
-    } else {
-      img.style.opacity = 0;
-    }
-  });
-});
+closeMenuBtn.addEvent
