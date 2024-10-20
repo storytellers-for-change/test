1335f29b60832f8fbcc2b752062a1376901d6668
storytellers-for-change <steffi.kohl@zuyd.nl>
2024-10-20 23:11:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,52 @@ const menu = document.querySelector('#menu');
 
 function openMenu() {
   menu.classList.add('open');
+  window.addEventListener('keyup', pressESC);
 }
 
 function closeMenu() {
   menu.classList.remove('open');
+  window.removeEventListener('keyup', pressESC);
 }
 
-openMenuBtn.addEventListener('click', openMenu);
-closeMenuBtn.addEventListener('click', closeMenu);
+openMenuBtn.addEventListener('click', () => {
+  handleViewTransition(openMenu);
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  handleViewTransition(closeMenu);
+});
+
+function handleViewTransition(callback) {
+  if (!document.startViewTransition) callback();
+  else document.startViewTransition(() => callback());
+}
+
+// Close Menu by Pressing Escape (ESC)
+function pressESC(e) {
+  if (e.key === 'Escape') {
+    handleViewTransition(closeMenu);
+  }
+}
+
+// Image Animation on Scroll
+const sections = document.querySelectorAll('.content-section');
+const images = document.querySelectorAll('.image');
+
+window.addEventListener('scroll', () => {
+  let index = 0;
+  sections.forEach((section, i) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= window.innerHeight / 2) {
+      index = i;
+    }
+  });
+
+  images.forEach((img, i) => {
+    if (i === index) {
+      img.style.opacity = 1;
+    } else {
+      img.style.opacity = 0;
+    }
+  });
+});
